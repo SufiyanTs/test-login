a26f3ea9e91ce7077b652b13a9b6c57abefb5944
SufiyanTs <ah.nursufyantsauri@gmail.com>
2022-07-11 14:37:26 +0700

    login dan log out

---
+++ project_4/build/web/WEB-INF/views/layout/navbar.jsp
@@ -0,0 +1,9 @@
+<header class="sticky-top">
+  <nav>
+    <ul>
+        <li><a href="product">Product</a></li>
+        <li><a href="create">Add item</a></li>
+        <li><a href="AboutPage.html">Order by chat</a></li>
+    </ul>
+  </nav>
+</header>
---
+++ project_4/src/java/Query/AdminQuery.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Query;
+
+/**
+ *
+ * @author fiyan
+ */
+public class AdminQuery {
+    public String getByUsername = "SELECT * FROM users WHERE username = ?";
+}
